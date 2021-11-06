d17b3646eb8c7f79be5e3fb0bbd413d691d6444e
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-05 20:19:02 -0500

    feat: :zap: update get listing route with optional filters (keywords and limit)
    still needs maxPrice filter

--- server/app/router.js
+++ server/app/router.js
@@ -14,10 +14,17 @@ router.get("/", (_, res) => {
 });
 
 // get all listings
-router.get("/listings", async (_, res) => {
-    const listingsData = await collection.find({}).toArray();
+router.get("/listings", async (req, res) => {
+    const listingsData = await collection.find(
+        { $or:[ 
+            {name: {$regex: (req.body.keywords? req.body.keywords : ""), $options: "i" }}, 
+            {description: {$regex: (req.body.keywords? req.body.keywords : ""), $options: "i"}},
+        ]})
+        .limit(req.body.limit? req.body.limit : 0)
+        .toArray();
     res.json(listingsData);
 });
+
 
 // get listing by id (dynamic route)
 router.get("/listings/:id", async (req, res) => {
